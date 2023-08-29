90f151d6160dc0ed6be2a6c6e9d8368271225822
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-08-29 20:21:50 +0200

    feat: implement 2nd extra

--- src/Book.ts
+++ src/Book.ts
@@ -1,25 +1,37 @@
 import { ISBN } from './ISBN';
+import { IsDateString, IsString } from 'class-validator';
 
-export interface Book {
+export class Book {
   /**
    * International Standard Book Number of the book
    */
+  @IsString()
   isbn: ISBN;
 
   /**
    * Title of the book
    */
+  @IsString()
   title: string;
 
   /**
    * Author of the book
    */
+  @IsString()
   author: string;
 
   /**
    * Date of publication of the book
    */
+  @IsDateString()
   date: string;
+
+  constructor(isbn: ISBN, title: string, author: string, date: string) {
+    this.isbn = isbn;
+    this.title = title;
+    this.author = author;
+    this.date = date;
+  }
 }
 
 export const compareWithTitle = (a: Book, b: Book): number => {
